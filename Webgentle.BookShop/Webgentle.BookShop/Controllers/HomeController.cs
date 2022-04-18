a49c308cb777f49c063012ff17c6523842ea0bc4
muhammad-zayn <xaynshaikh@gmail.com>
2022-04-19 04:01:28 +0500

    Added MVC To Application

---
+++ Webgentle.BookShop/Webgentle.BookShop/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Webgentle.BookShop.Controllers
+{
+    public class HomeController : Controller   // to make it an MVC controller we inherited it with controller class
+    {
+        public string index() 
+        { 
+            
+            return "i am in home controller";
+        }
+    }
+}
--- Webgentle.BookShop/Webgentle.BookShop/Startup.cs
+++ Webgentle.BookShop/Webgentle.BookShop/Startup.cs
@@ -23,7 +23,8 @@ namespace Webgentle.BookShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +38,54 @@ namespace Webgentle.BookShop
 
             app.UseRouting();
 
+            
             app.UseEndpoints(endpoints =>
             {
-            endpoints.MapGet("/", async context =>
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.Map("/", async context =>
+                //{
+                        
+                //    await context.Response.WriteAsync(env.EnvironmentName);
+                //});
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/store", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Zain"); 
+                    await context.Response.WriteAsync("Welcome To Store");
                 });
             });
 
+
+
+            //ye new middle wear creat horha hai created by zain for practice
+            //app.Use(async (context,next) => 
+            //{
+            //    await context.Response.WriteAsync("Hello from the first middle wear");
+
+            //    await next();
+
+            //    await context.Response.WriteAsync("Hello First M W");
+            //});
+
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From The Second Midlle Wear");
+
+            //    await next();
+
+            //    await context.Response.WriteAsync("Hello Second M W");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From The 3rd Midlle Wear");
+
+            //    next();
+
+            //});
+            //ye new middle wear creat horha hai created by zain for practice
 
             //if (env.IsDevelopment())
             //{
